6139383ef721d2c0aa5060caade287f8f70b28c7
Neelam <neelammujawar12@gmail.com>
2024-01-04 13:29:09 +0530

    add Hotel in a Hotel Reservation System with Name and rates for Regular Customer...

--- HotelRegistration/src/main/java/org/com/in/HotelRegistration.java
+++ HotelRegistration/src/main/java/org/com/in/HotelRegistration.java
@@ -1,10 +1,35 @@
 package org.com.in;
 
+import java.util.*;
+
+class Hotel {
+    String name;                                                                        	//variable declaration
+    double regularRate;
+
+    public Hotel(String name, double regularRate) {                                           //constructor calling
+        this.name = name;
+        this.regularRate = regularRate;
+    }
+
+    @Override                                   												//tostring method
+    public String toString() {
+        return "Hotel Name: " + name + ", Regular Rate: $" + regularRate;
+    }
+}
+
 public class HotelRegistration {
+    public static void main(String[] args) {
+        ArrayList<Hotel> hotelsList = new ArrayList<>();                                        //arraylist of hotel class
 
-	public static void main(String[] args) {
-	System.out.println("Welcome to Hotel Registration app");
-
-	}
-
+        // Hardcoded hotels
+        hotelsList.add(new Hotel("Lakewood", 80.0));
+        hotelsList.add(new Hotel("Bridgewood", 110.0));
+        hotelsList.add(new Hotel("Ridgewood", 100.0));
+        
+                                                                                               		//printing using for each loop
+        System.out.println("Hotel Information:");
+        for (Hotel hotel : hotelsList) {
+            System.out.println(hotel);
+        }
+    }
 }
