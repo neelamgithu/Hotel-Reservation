da9751d0554cef0a60bfb6b4b7dc2d0175245d43
Neelam <neelammujawar12@gmail.com>
2024-01-09 15:05:35 +0530

    Adding rewarded coustmer values

--- HotelRegistration/src/main/java/org/com/in/HotelRegistration.java
+++ HotelRegistration/src/main/java/org/com/in/HotelRegistration.java
@@ -1,117 +1,99 @@
 //In this program, a new method calculateTotalCost is added to the Hotel class to calculate the total cost for a given number of days.
 //The findCheapestHotel method is then used to find the hotel with the minimum total cost for the specified date range.
 //The program takes the number of days as input and displays the cheapest hotel along with the total cost.
-
 package org.com.in;
 
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 class Hotel {
     String name;
-    double regularRate;
-   double WeekendRates;
-   int rating;
+    double weekdayRate;
+    double weekendRate;
+    double rewardWeekdayRate;
+    double rewardWeekendRate;
+    int rating;
     // Constructor to initialize hotel name and regular rate
-    public Hotel(String name, double regularRate, double WeekendRates, int rating) {
+
+
+    public Hotel(String name, double weekdayRate, double weekendRate, int rating,double rewardWeekdayRate ,
+    double rewardWeekendRate) {
         this.name = name;
-        this.regularRate = regularRate;
-        this.WeekendRates=WeekendRates;
-        this.rating=rating;
+        this.weekdayRate = weekdayRate;
+        this.weekendRate = weekendRate;
+        this.rating = rating;
+        this.rewardWeekdayRate =rewardWeekdayRate ;
+        this. rewardWeekendRate=rewardWeekendRate;
     }
 
-    // Override toString method to provide a string representation of the hotel
-    @Override
-    public String toString() {
-        return "Hotel Name: " + name + ", Regular Rate: $" + regularRate + "WeekendRate: $" + WeekendRates ;
-    }
-
-    // Method to calculate the total cost for a given number of days
-    public double calculateTotalCost(int numDays) {
-        return regularRate * numDays;
-    }
     public double getRate(boolean isWeekend) {
-        return isWeekend ? WeekendRates : regularRate;
+        return isWeekend ? weekendRate : weekdayRate;
     }
 
-	public int getRating() {
-		
-		return rating;
-	}
+    public int getRating() {
+        return rating;
+    }
 }
 
 public class HotelRegistration {
     public static void main(String[] args) {
-        // ArrayList to store hotel objects
-        ArrayList<Hotel> hotelsList = new ArrayList<>();
+        // Create a list of hotels
+        ArrayList<Hotel> hotels = new ArrayList<>();
+        
+        // Add hotels with their rates and ratings
+        hotels.add(new Hotel("Lakewood", 110 ,90, 3 ,80,80));
+        hotels.add(new Hotel("Bridgewood", 160.0 ,60,4,110 ,50));
+        hotels.add(new Hotel("Ridgewood", 220.0,150,5,100,40));
+     
 
-        // Hardcoded hotels with their regular rates and weekend days rates
-        hotelsList.add(new Hotel("Lakewood", 110 ,90,3));
-        hotelsList.add(new Hotel("Bridgewood", 160.0 ,60,4));
-        hotelsList.add(new Hotel("Ridgewood", 220.0,150,5));
-       
-
-        // Display hotel information
-        System.out.println("Hotel Information:");
-        for (Hotel hotel : hotelsList) {
-            System.out.println(hotel);
-        }
+        // Number of days
 
         // User input for the number of days
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of days for the date range: ");
-        int numDays = scanner.nextInt();}
-          
-        
-        private static double calculateTotalCost(Hotel hotel, int numDays, ArrayList<Hotel> hotelsList) {
-            double totalCost = 0;
+        int numberOfDays = scanner.nextInt();
+       
 
-            for (int day = 1; day <= numDays; day++) {
-                boolean isWeekend = (day % 7 == 1) || (day % 7 == 7); // Sunday or Saturday
-                totalCost += hotel.getRate(isWeekend);
-            }
+        // Find the cheapest and highest-rated hotel
+        Hotel cheapestHotel = findCheapestHotel(hotels, numberOfDays);
+        Hotel highestRatedHotel = findHighestRatedHotel(hotels);
 
-            return totalCost;
-        
+        // Display the result
+        if (cheapestHotel != null) {
+            System.out.println("Cheapest hotel for the given number of days: " + cheapestHotel.name);
+            System.out.println("Total cost: " + calculateTotalCost(cheapestHotel, numberOfDays));
+        } else {
+            System.out.println("No hotels found for the given number of days.");
+        }
 
-        // Find the cheapest hotel for the given date range
-        Hotel cheapestHotel = findCheapestHotel(   hotelsList, int numDays);
-
-        // Display the cheap hotels for week days
-        System.out.println("The cheapest hotel for regular coustmer on weekdays for the given date range is: " + cheapestHotel.name);
-        System.out.println("Total cost or regular coustmer on weekdays: $" + cheapestHotel.calculateTotalCost(numDays));
+        if (highestRatedHotel != null) {
+            System.out.println("Highest-rated hotel: " + highestRatedHotel.name);
+            System.out.println("Rating: " + highestRatedHotel.getRating());
+        } else {
+            System.out.println("No hotels found with ratings.");
+        }
     }
 
-    // Method to find the cheapest hotel for a given date range
-    private static Hotel findCheapestHotel(ArrayList<Hotel> hotelsList, int numDays) {
-//        if (hotelsList.isEmpty()) {
-//            return null; // Return null if the list is empty
-//        }
+    private static Hotel findCheapestHotel(ArrayList<Hotel> hotels, int numberOfDays) {
+        double minCost = Double.MAX_VALUE;
+        Hotel cheapestHotel = null;
 
-        // Initialize the first hotel as the cheapest
-        Hotel cheapestHotel = hotelsList.get(0);
-double minCost=cheapestHotel.calculateTotalCost(numDays);
-
-        // Iterate through the list to find the hotel with the minimum total cost
-        for (Hotel hotel : hotelsList) {
-            double totalCost = hotel.calculateTotalCost(numDays);
-            if (totalCost < minCost) {
+        for (Hotel hotel : hotels) {
+            double totalCost = calculateTotalCost(hotel, numberOfDays);
+            if (totalCost < minCost || (totalCost == minCost && hotel.getRating() > cheapestHotel.getRating())) {
                 minCost = totalCost;
                 cheapestHotel = hotel;
             }
         }
 
-        return cheapestHotel; // Return the cheapest hotel
-        // this logic assumes Sunday as the first day of the week. If your week starts from Monday
-        
+        return cheapestHotel;
     }
-    
-    
-    private static Hotel findHighestRatedHotels(ArrayList<Hotel> hotelsList , int rating) {
-        int maxRating = 0;
+
+    private static Hotel findHighestRatedHotel(ArrayList<Hotel> hotels) {
+        int maxRating = Integer.MIN_VALUE;
         Hotel highestRatedHotel = null;
 
-        for (Hotel hotel : hotelsList) {
+        for (Hotel hotel : hotels) {
             if (hotel.getRating() > maxRating) {
                 maxRating = hotel.getRating();
                 highestRatedHotel = hotel;
@@ -120,9 +102,8 @@ double minCost=cheapestHotel.calculateTotalCost(numDays);
 
         return highestRatedHotel;
     }
-} 
-/*
- private static double calculateTotalCost(Hotel hotel, int numDays) {
+
+    private static double calculateTotalCost(Hotel hotel, int numberOfDays) {
         double totalCost = 0;
 
         for (int day = 1; day <= numberOfDays; day++) {
@@ -130,4 +111,6 @@ double minCost=cheapestHotel.calculateTotalCost(numDays);
             totalCost += hotel.getRate(isWeekend);
         }
 
-        return totalCost;*/
+        return totalCost;
+    }
+}
